eaa707ad2e54cf783d05bf9703e2d5845f8a107e
svasenkov <aanher@gmail.com>
2021-05-31 21:03:51 +0300

    simple browserstack example with Selenide

---
+++ src/test/java/tests/BrowserStackAndroidSelenideTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import drivers.BrowserstackMobileDriver;
+import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class BrowserStackAndroidSelenideTests {
+
+    @Test
+    void searchTest() {
+        Configuration.browser = BrowserstackMobileDriver.class.getName();
+        Configuration.startMaximized = false;
+        Configuration.browserSize = null;
+        Configuration.timeout = 10000;
+
+        open();
+        $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).val("BrowserStack");
+        $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container")).shouldHave(sizeGreaterThan(0));
+
+        closeWebDriver();
+    }
+}
