b8825b8bd0070c37cbeeadd6a33eae7e098cb6bc
elenaAeternanox <antares87@mail.ru>
2022-01-11 22:39:45 +0300

    Fix BrowserStackAndroidSampleTests

--- src/test/java/com/github/elenaAeternaNox/browserstack/tests/BrowserStackAndroidSampleTests.java
+++ src/test/java/com/github/elenaAeternaNox/browserstack/tests/BrowserStackAndroidSampleTests.java
@@ -3,6 +3,7 @@ package com.github.elenaAeternaNox.browserstack.tests;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,7 +13,9 @@ import java.net.URL;
 import java.util.List;
 
 public class BrowserStackAndroidSampleTests {
-    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+    @Test
+    void SearchTest() throws MalformedURLException, InterruptedException {
 
         DesiredCapabilities caps = new DesiredCapabilities();
 
